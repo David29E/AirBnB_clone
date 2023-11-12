118785ecf32f4cc211343104289e57a55c2a28ff
Dawit Alemayehu <dawitalemayehu234@gmail.com>
2023-11-12 23:40:19 +0300

    create class city

---
+++ models/city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+    Create class City
+"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Represent a City"""
+
+    state_id = ""
+    name = ""
